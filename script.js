e5e26c9e64834e5792c7205b0fb074acfa4b62a5
yusufarnesa-cpu <yusufarnesa@gmail.com>
2026-09-04 07:38:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Learning Hub berhasil terhubung dengan JavaScript.");
+const SUPABASE_URL = "https://hqzishiazabguknallei.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "sb_publishable_r8rspBje6vjTvUsF9lKYzA_SakK9CD5";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
+
+console.log("Supabase berhasil terhubung.");
+supabaseClient
+  .from("categories")
+  .select("*")
+  .limit(1)
+  .then(({ data, error }) => {
+    if (error) {
+      console.error("Koneksi Supabase gagal:", error);
+    } else {
+      console.log("Koneksi Supabase berhasil:", data);
+    }
+  });
