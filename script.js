ea3162edf6a546478abcc15a2f927b660c2799ce
yusufarnesa-cpu <yusufarnesa@gmail.com>
2026-09-04 08:08:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://hqzishiazabguknallei.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://hqzishiazabguknallei.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_r8rspBje6vjTvUsF9lKYzA_SakK9CD5";
 
 const supabaseClient = window.supabase.createClient(
